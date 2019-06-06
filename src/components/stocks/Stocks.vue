2c4849ae3c6b2b885d0e2503e97bef8bbf17607f
unknown <abagga@elyosdigital.com>
2019-06-06 01:21:17 +0100

    Add Vuex and Setting up the Vuex State Management

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -9,18 +9,13 @@
 import stock from "./Stock.vue";
 
 export default {
-  data() {
-    return {
-      stocks: [
-        { id: 1, name: "BMW", price: 110 },
-        { id: 2, name: "Google", price: 210 },
-        { id: 3, name: "FB", price: 180 },
-        { id: 4, name: "Twitter", price: 5 }
-      ]
-    };
-  },
   components: {
     appStock: stock
+  },
+  computed : {
+    stocks(){
+      return this.$store.getters.stocks;
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router';
 
 import App from './App.vue'
-import { routes } from './routes.js';
-
+import { routes } from './routes';
+import store from './store/store';
 
 Vue.use(VueRouter);
 
@@ -15,5 +15,6 @@ const router = new VueRouter ({
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
